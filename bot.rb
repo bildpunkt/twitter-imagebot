f65e81ccc3daeb4fa435b360e36332ef4d997466
pixeldesu <pixeldesu@nerdwerks.org>
2014-11-07 18:07:55 +0100

    add configuration files and beginning code

--- bot.rb
+++ bot.rb
@@ -1,1 +1,22 @@
- 
+#!/usr/bin/env ruby
+require "yaml"
+require "twitter"
+require "activesupport"
+
+# config file
+conf = YAML.load_file File.expand_path(".", "config.yml")
+
+# image file
+img = YAML.load_file File.expand_path(".", "config.yml")
+
+# Twitter client configuration
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key = conf['twitter']['consumer_key']
+  config.consumer_secret = conf['twitter']['consumer_secret']
+  config.access_token = conf['twitter']['access_token']
+  config.access_token_secret = conf['twitter']['access_token_secret']
+end
+
+loop do
+  sleep conf['sleep_time'].minutes
+end
