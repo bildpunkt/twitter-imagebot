14d71ced4014ca450fbd1af5799ec1c8c69441f3
Andreas N. <pixeldesu@nerdwerks.org>
2014-11-20 17:04:35 +0100

    added rerolling if bot gets same image twice in a row

--- bot.rb
+++ bot.rb
@@ -3,7 +3,7 @@ require "yaml"
 require "twitter"
 
 # version
-version = "v1.0.0"
+version = "v1.1.0"
 
 # config file
 conf = YAML.load_file File.expand_path(".", "config.yml")
@@ -28,15 +28,25 @@ puts "----------------------------------------"
 loop do
   begin
     post_img = img.sample
-    if File.exists? File.expand_path("../#{conf['image_directory']}#{post_img['location']}", __FILE__)
-      client.update_with_media "#{post_img['name']} | #{post_img['source']}", File.new("#{conf['image_directory']}#{post_img['location']}")
-      puts "\033[32;1m[#{Time.new.to_s}] Successfully posted image '#{post_img['name']}'\033[0m"
+    if last_img != post_img['location']
+      if File.exists? File.expand_path("../#{conf['image_directory']}#{post_img['location']}", __FILE__)
+        client.update_with_media "#{post_img['name']} | #{post_img['source']}", File.new("#{conf['image_directory']}#{post_img['location']}")
+        puts "\033[32;1m[#{Time.new.to_s}] Successfully posted image '#{post_img['name']}'\033[0m"
+        last_img = post_img['location']
+      else
+        puts "\033[31;1m[#{Time.new.to_s}] No image for #{post_img['name']} found!\033[0m"
+      end
     else
-      puts "\033[31;1m[#{Time.new.to_s}] No image for #{post_img['name']} found!\033[0m"
+      puts "\033[31;1m[#{Time.new.to_s}] Generated '#{post_img['name']}' again. Re-rolling!"
+      reroll = true
     end
   rescue Exception => e
     puts "\033[31;1m[#{Time.new.to_s}] #{e.message}\033[0m"
   end
-  # since activesupport doesn't like my bot, lets use this hack
-  sleep conf['sleep_time'].to_i * 60
+  if reroll
+    sleep 1
+    reroll = false
+  else
+    sleep conf['sleep_time'].to_i * 60
+  end
 end
